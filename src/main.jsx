236be2a06adcab0a1ad8181502fb27fb9dc3bd13
matheusvmd <matheus.vicco173016@gmail.com>
2025-11-10 00:18:37 -0300

    auth0 trocado

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
-      domain="dev-vlbhbshl7b1pxe8e.us.auth0.com"
-      clientId="4rwe12yYJbxAD122EDkjl61unHBaHFL0"
+      domain="dev-ksti8ah1bi3ygj1k.us.auth0.com"
+      clientId="b0zWOQAhMycakIlrDKIOBAKooOOzTJw8"
       authorizationParams={{
-        audience: "https://dev-vlbhbshl7b1pxe8e.us.auth0.com/api/v2/",
+        audience: "https://dev-ksti8ah1bi3ygj1k.us.auth0.com/api/v2/",
         redirect_uri: window.location.origin
       }}
     >
